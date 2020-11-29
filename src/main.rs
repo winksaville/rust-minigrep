706615602d8a11dcb8c9d5b4a05c7970155b6875
Wink Saville <wink@saville.com>
2020-11-28 16:29:10 -0800

    Add poem.txt
    And change main to read the filename parameter into contents.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,5 +7,8 @@ fn main() {
     let query = &args[1];
     let filename = &args[2];
 
-    println!("query: {} filename: {}", query, filename);
+    println!("query: {}\nfilename: {}", query, filename);
+
+    let contents = fs::read_to_string(filename).expect("a problem reading");
+    println!("contents:\n{}", contents);
 }
