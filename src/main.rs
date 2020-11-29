d9195061a42795ea6bb4a742b9f33b586cd7e050
Wink Saville <wink@saville.com>
2020-11-28 16:36:36 -0800

    Parse arguments in its own function

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,17 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     println!("query: {}\nfilename: {}", query, filename);
 
     let contents = fs::read_to_string(filename).expect("a problem reading");
     println!("contents:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+    
+    (query, filename)
 }
