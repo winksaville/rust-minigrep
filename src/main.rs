15d316a3d0c38fc810a0a8286e1375820e4d7313
Wink Saville <wink@saville.com>
2020-11-28 17:17:54 -0800

    Change run to return Result
    And have main handle errors from run the same was as it
    does for Config::new.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,13 +13,17 @@ fn main() {
 
     println!("query: {}\nfilename: {}", config.query, config.filename);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
 
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.filename)
-        .expect("a problem reading");
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.filename)?;
     println!("contents:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
