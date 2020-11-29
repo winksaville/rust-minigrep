f4888aad9a6273adbf41ef0a3c90ba34156017e7
Wink Saville <wink@saville.com>
2020-11-28 16:52:36 -0800

    Change parse_config to new
    It's now a method on Config, which is reasonable and we also
    add some error checking. But that will be further improved next.

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("query: {}\nfilename: {}", config.query, config.filename);
 
-    let contents = fs::read_to_string(config.filename).expect("a problem reading");
+    let contents = fs::read_to_string(config.filename)
+        .expect("a problem reading");
     println!("contents:\n{}", contents);
 }
 
@@ -17,9 +18,14 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("execting 2 arguments only found {}", args.len())
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        
+        Config { query, filename }
+    }
 }
