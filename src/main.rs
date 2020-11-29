b38327ebd29a7dc593a62233d7b7f3345d49d8cb
Wink Saville <wink@saville.com>
2020-11-28 17:06:49 -0800

    Change Config::new to return a Result

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("query: {}\nfilename: {}", config.query, config.filename);
 
@@ -19,13 +23,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("execting 2 arguments only found {}", args.len())
+            return Err("execting 2 arguments");
         }
         let query = args[1].clone();
         let filename = args[2].clone();
         
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
